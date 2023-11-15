150df01b526a429c8ef1836770d973ec3bef5684
Kenneth <125ksantoslopez@frhsd.com>
2023-11-15 09:27:21 -0500

    hi

--- magic 8/js/magic.js
+++ magic 8/js/magic.js
@@ -1,4 +1,36 @@
-let userName = "";
+let userName = 'tigre';
+const userQuestion = '?';
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
 
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
-userName ? console.log("Hello, " + userName + "!") : console.log("Hello!");
+console.log(`${userName} asked: ${userQuestion}`);
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'caannot predict now';
+    break;
+  case 4:
+    eightBall = 'ddo not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'signs point to yes';
+}
+
+console.log(`Magic Eightball: ${eightBall}`); 
