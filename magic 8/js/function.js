95f15e5dbe370c0529f43ecf31e5c4f5ee973358
Kenneth <125ksantoslopez@frhsd.com>
2023-12-01 07:34:23 -0500

    push

---
+++ magic 8/js/function.js
@@ -0,0 +1,21 @@
+let compChoice;
+let userChoice;
+let randomNum;
+let numLimit;
+
+// Random numbers (Inclusive-Exclusive Range)
+// Function: Generate a random number between 0 and 3
+function getRandomNum(low, high){
+    return Math.floor(math.random() * high);
+}
+// include 3 exclude 10
+// 3-9
+3-10
+0.7
+randomNum = getRandomNum(0, 3);
+// TEST:
+console.log(randomNum);
+
+// Switch statements 
+// Comparisons
+// console Logs
---
+++ magic 8/js/rpc.js
@@ -0,0 +1,9 @@
+switch(choice) {
+    case 0:
+        object = "rock";
+        break;
+        case 1:
+            object="paper";
+            break;
+            case 2
+}
